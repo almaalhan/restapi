6aa0ed96efbbfce5faa82426a338563232f92ca1
almaalhana <almaalhana1@gmail.com>
2021-11-20 16:32:20 +0700

    dsfdsf

--- controller.js
+++ controller.js
@@ -20,3 +20,31 @@ exports.get_user_data = function (req, res) {
         }
     });
 }
+
+// get data user by id
+exports.get_data_user_by_id = function (req, res) {
+    var id = req.body.id;
+    // console.log(id);
+    connection.query("select nama,nim from user where username = '" + id + "'", function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+}
+
+// tambah data user
+exports.add_data_user = function (req, res) {
+    var nama = req.body.nama;
+    var username = req.body.username;
+    console.log(nama);
+    console.log(username);
+    connection.query("INSERT INTO user (nama,username,nim) VALUES(?,?,?)", [nama, username, username], function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok("User Berhasil ditambahkan", res);
+        }
+    });
+}
--- router.js
+++ router.js
@@ -5,4 +5,6 @@ module.exports = function (app) {
 
     app.route('/').get(json.index);
     app.route('/get_user_data').get(json.get_user_data);
+    app.route('/get_data_user_by_id').post(json.get_data_user_by_id);
+    app.route('/add_data_user').post(json.add_data_user);
 }
